d69f908c03a6ad6546e752cb13c80482058794bf
hivernal <you@example.com>
2024-03-09 20:13:26 +0700

    change scripts

--- brightness.sh
+++ brightness.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-BRIGHTNESS=
-if [[ $1 == "up" ]] then
-  BRIGHTNESS=$(brightnessctl s +5% | grep -Eo "[0-9]*%")
-elif [[ $1 == "down" ]]; then
-  BRIGHTNESS=$(brightnessctl s 5%- | grep -Eo "[0-9]*%")
+SCRIPT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ICON="${SCRIPT_DIR}/icons/icons8-sun.svg"
+
+if [[ "$1" == "up" ]] then
+  brightness="$(brightnessctl s +5% | grep -Eo '[0-9]{1,3}%')"
+elif [[ "$1" == "down" ]]; then
+  brightness="$(brightnessctl s 5%- | grep -Eo '[0-9]{1,3}%')"
+else
+  brightness="$(brightnessctl | grep -Eo '[0-9]{1,3}%')"
 fi
 
-BRIGHTNESS=${BRIGHTNESS%%%}
-ICON=$HOME/.config/hypr/icons/icons8-sun.svg
+brightness=${brightness%%%}
 
-dunstify -t 2000 -i $THEME/$ICON -h string:x-canonical-private-synchronous:brightness "$BRIGHTNESS%" -h int:value:$BRIGHTNESS
+dunstify -t 2000 -i "${ICON}" -h string:x-canonical-private-synchronous:brightness "${brightness}%" -h int:value:${brightness}
